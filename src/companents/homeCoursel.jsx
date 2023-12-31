2ffa29644845eb268d7dfe4a0e7198382a488ef4
MustafaMert <mustafamert1011@gmail.com>
2023-12-31 19:05:36 +0300

    Coursel ayarlamaları yapıldı

--- src/companents/homeCoursel.jsx
+++ src/companents/homeCoursel.jsx
@@ -1,33 +1,90 @@
 import { Button } from "@material-tailwind/react";
 import CourselData from "../data/courselData";
+import { useState, useEffect } from "react";
+
+function HomeCourselTemplate({ img, title, desc, button, onSelectHandel, onStopHandel }) {
+    return (
+        <>
+            <div
+                onMouseEnter={() => onStopHandel("pause")}
+                onMouseLeave={() => onStopHandel("resume")}
+                className="bg-[#ffff] rounded-md bg-no-repeat bg-cover min-h-[20vh] text-center"
+                style={{
+                    backgroundPosition: "center center",
+                    backgroundImage: "url('" + img + "')",
+                }}
+            >
+                <div className="py-12 shadow-xl">
+                    <div className="md:flex md:mx-5 mx-0 bg-white">
+                        <div className="md:w-2/3 p-4 sm:w-full sm:block">
+                            <img src={img} className="rounded-xl m-auto" />
+                        </div>
+                        <div className="md:w-1/3 p-4 text-2xl">
+                            <h2 className="pt-10">{title}</h2>
+                            <p className="font-bold pt-4">{desc}</p>
+                            <Button className="md:mt-[25%] sm:mt-5">{button}</Button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex justify-center pb-3" style={{ backdropFilter: "blur(25px)" }} >
+                    {CourselData.map((item, index) => {
+                        return (<div key={index} className="w-24 h-12 bg-blue-500 m-2" onClick={() => { onSelectHandel(index) }}>
+                            <img src={item.img} alt="" />
+                        </div>)
+                    })}
+
+                </div>
+            </div>
+        </>
+    );
+}
 
 function HomeCoursel() {
 
+    const [selectCoursel, setSelectCoursel] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
 
+    useEffect(() => {
+        let intervalId;
 
-    console.log(CourselData)
+        const handleInterval = () => {
+            if (!isPaused) {
+                if (selectCoursel === CourselData.length - 1) {
+                    setSelectCoursel(0);
+                } else {
+                    setSelectCoursel((prevSelect) => prevSelect + 1);
+                }
+            }
+        };
 
-    return (<>
-        <div className="bg-[#ffff] rounded-md bg-no-repeat bg-cover min-h-[20vh] text-center" style={{ backgroundPosition: "center center", backgroundImage: "url('https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2130_20231208181518.jpeg')" }}>
-            <div className="py-12 shadow-xl ">
+        intervalId = setInterval(handleInterval, 4000);
 
-            <div class="flex md:mx-5  mx-0 bg-white" >
-                <div class="w-1/3 p-4  text-2xl">
-                    <h2 className="pt-10">Yeni yıl gelmeden</h2>
-                    <p className="font-bold pt-4">fırsatlar geldi</p>
-                    <Button className="mt-[25%]">Alışverişe Başla</Button>
-                </div>
-                <div class="w-2/3 p-4 ">
-                    <img src="https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2130_20231208181518.jpeg" className="rounded-xl m-auto" />
-                </div>
-            </div>
-            </div>
-        </div>
+        return () => clearInterval(intervalId);
+    }, [isPaused, selectCoursel]);
 
+    const handleMouseAction = (action) => {
+        if (action === "pause") {
+            setIsPaused(true);
+        } else if (action === "resume") {
+            setIsPaused(false);
+        }
+    };
 
-
-
-    </>);
+    return (
+        <>
+            <HomeCourselTemplate
+                title={CourselData[selectCoursel].text}
+                desc={CourselData[selectCoursel].desc}
+                button={CourselData[selectCoursel].button}
+                img={CourselData[selectCoursel].img}
+                onSelectHandel={(e) => {
+                    setSelectCoursel(e);
+                }}
+                onStopHandel={(e) => { handleMouseAction(e) }}
+            />
+        </>
+    );
 }
 
 export default HomeCoursel;
--- src/data/courselData.jsx
+++ src/data/courselData.jsx
@@ -1,11 +1,33 @@
 const CourselData = [
     {
-        text: "deneme yazısı",
+        text: "Çok süper indirimlerle",
+        desc: "çok süper fırsatlar",
+        button: "Alışverişe başla",
         img: "https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2125_20231023121055.png"
     },
     {
-        text: "deneme yazısı",
+        text: "Yeni yıl gelmeden",
+        desc: "fırsatlar geldi",
+        button: "Alışverişe başla",
         img: "https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2130_20231208181518.jpeg"
+    },
+    {
+        text: "Bu İndirim şimdi var",
+        desc: "sonra yok",
+        button: "Acele et Kaçırma",
+        img: "https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2089_20231201171246.jpeg"
+    },
+    {
+        text: "Çok satan tabletler",
+        desc: "süper fırsatlar",
+        button: "Alışverişe başla",
+        img: "https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2115_20231130142731.jpeg"
+    },
+    {
+        text: "Küçük ev aletleri",
+        desc: "yılın son fırsatları",
+        button: "Acele et Kaçırma",
+        img: "https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2113_20231222143128.jpeg"
     }
 ]
 
